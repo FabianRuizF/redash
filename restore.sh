a7eace397133aaa7a6160d4fe9c17c00c60a3713
FabianRuizF <fabian.ruiz@alumnos.uach.cl>
2023-04-08 20:50:12 -0400

    Create restore.sh

---
+++ restore.sh
@@ -0,0 +1,8 @@
+cp redash_env /opt/redash/env
+cd data
+docker-compose up -d
+cd ..
+sleep 10
+docker cp redash.sql data_postgres_1:/
+docker exec data_postgres_1 /usr/local/bin/psql -h localhost -U postgres -f redash.sql
+docker exec -it data_server_1  /app/manage.py users password $1 $2
